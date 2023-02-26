a779d617b52ec5fdb0dfa26fd0b1b6e55e3189d3
bennibm <benni.mehlbm@gmail.com>
2023-02-26 16:27:46 +0100

    feat: keyboard tilted

--- src/components/dynamic/ThreeKeyboard.tsx
+++ src/components/dynamic/ThreeKeyboard.tsx
@@ -1,16 +1,15 @@
-import { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, CameraControls } from '@react-three/drei';
 import Keyboard from './Keyboard';
 
 export default function ThreeKeyboard() {
   return (
-    <Canvas style={{ height: '50vh' }} camera={{ position: [2, 3, 5] }}>
-      <ambientLight intensity={0.5} />
+    <Canvas style={{ height: '50vh' }} camera={{ position: [0, 0, 3] }}>
+      <ambientLight intensity={0.3} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Keyboard position={[0, 0, 0]} />
-      <OrbitControls />
+      <Keyboard position={[0, 0, 0]} rotation={[0.5, -0.15, 0]} />
+      {/* <OrbitControls /> */}
     </Canvas>
   );
 }
