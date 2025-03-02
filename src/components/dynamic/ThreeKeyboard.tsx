a707ea6555afee233be7d5192f347ec04b50cf25
Benjamin Mehl <Benni.mehlBM@gmail.com>
2025-03-02 17:14:38 +0100

    better light on keyboard

--- src/components/dynamic/ThreeKeyboard.tsx
+++ src/components/dynamic/ThreeKeyboard.tsx
@@ -4,9 +4,9 @@ import Keyboard from './Keyboard';
 export default function ThreeKeyboard() {
   return (
     <Canvas style={{ height: '36vw' }} camera={{ position: [0, 0, 3.4] }}>
-      <ambientLight intensity={0.3} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
+      <ambientLight intensity={0.2} />
+      <directionalLight position={[-5, 3, 2]} intensity={1.5} castShadow />
+      <spotLight position={[-3, 2, 1]} angle={0.3} penumbra={0.8} intensity={0.8} />
       <Keyboard position={[0, 0, 0]} rotation={[0.5, -0.15, 0]} />
       {/* <OrbitControls /> */}
     </Canvas>
