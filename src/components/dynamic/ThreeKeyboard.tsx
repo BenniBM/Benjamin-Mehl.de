5b42ed261ad6613e5cda92ce7fdc55fe42cd1748
Benjamin Mehl <Benni.mehlBM@gmail.com>
2025-03-03 18:23:24 +0100

    keyboard on mobile

--- src/components/dynamic/ThreeKeyboard.tsx
+++ src/components/dynamic/ThreeKeyboard.tsx
@@ -16,11 +16,11 @@ export default function ThreeKeyboard() {
   }, []);
 
   return (
-    <Canvas className="!h-[40vh] md:!h-[70vh] " camera={{ position: [0, 0, isMobile ? 2.8 : 3.4] }}>
+    <Canvas className="!h-[30vh] md:!h-[70vh] " camera={{ position: [0, 0, isMobile ? 2.8 : 3.4] }}>
       <ambientLight intensity={0.2} />
       <directionalLight position={[-5, 3, 2]} intensity={1.5} castShadow />
       <spotLight position={[-3, 2, 1]} angle={0.3} penumbra={0.8} intensity={0.8} />
-      <Keyboard position={[0, 1, 0]} rotation={[0.5, -0.15, 0]} />
+      <Keyboard position={[0, 1, isMobile ? -0.2 : 0]} rotation={[0.5, -0.15, 0]} />
     </Canvas>
   );
 }
