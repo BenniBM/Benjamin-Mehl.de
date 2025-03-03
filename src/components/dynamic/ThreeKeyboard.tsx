19ce98305e5431a2a79a124b1086d2427dcb3e06
Benjamin Mehl <Benni.mehlBM@gmail.com>
2025-03-03 18:21:58 +0100

    Keyboard on mobile

--- src/components/dynamic/ThreeKeyboard.tsx
+++ src/components/dynamic/ThreeKeyboard.tsx
@@ -1,14 +1,26 @@
 import { Canvas } from '@react-three/fiber';
 import Keyboard from './Keyboard';
+import { useEffect, useState } from 'react';
 
 export default function ThreeKeyboard() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
   return (
-    <Canvas style={{ height: '36vw' }} camera={{ position: [0, 0, 3.4] }}>
+    <Canvas className="!h-[40vh] md:!h-[70vh] " camera={{ position: [0, 0, isMobile ? 2.8 : 3.4] }}>
       <ambientLight intensity={0.2} />
       <directionalLight position={[-5, 3, 2]} intensity={1.5} castShadow />
       <spotLight position={[-3, 2, 1]} angle={0.3} penumbra={0.8} intensity={0.8} />
-      <Keyboard position={[0, 0, 0]} rotation={[0.5, -0.15, 0]} />
-      {/* <OrbitControls /> */}
+      <Keyboard position={[0, 1, 0]} rotation={[0.5, -0.15, 0]} />
     </Canvas>
   );
 }
